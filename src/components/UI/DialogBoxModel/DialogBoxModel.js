aaf222e181c48655978be45833c081b757f7501a
madhur <madhurdaphnis@gmail.com>
2019-09-02 03:51:11 +0530

    Added should component update

--- src/components/UI/DialogBoxModel/DialogBoxModel.js
+++ src/components/UI/DialogBoxModel/DialogBoxModel.js
@@ -1,19 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import style from './DialogBoxModel.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const dialogBox = (props) => (
-    <Fragment>
-        <Backdrop hideBackdrop={props.hideSummary} isVisible={props.show}/>
-        <div className={style.Modal}
-            style={
-                {
-                    transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-                    opacity: props.show ? "1" : "0"
-                }}>
-            {props.children}
-        </div>
-    </Fragment>
-);
+class DialogBox extends Component{
+    shouldComponentUpdate=(nextProps, nextState)=>{
+        return nextProps.show!==this.props.show;
+    }
+    render(){
 
-export default dialogBox;
+        return (
+            <Fragment>
+                <Backdrop hideBackdrop={this.props.hideSummary} isVisible={this.props.show}/>
+                <div className={style.Modal}
+                    style={
+                        {
+                            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                            opacity: this.props.show ? "1" : "0"
+                        }}>
+                    {this.props.children}
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default DialogBox;
